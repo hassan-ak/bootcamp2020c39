afc66049558ef3537b92799791a7c67ef8afe816
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-30 19:14:26 +0500

    Delete old files

--- packages/www/gatsby-ssr.js
+++
@@ -1,3 +0,0 @@
-const wrapRootElement = require("./wrap-root-element")
-
-exports.wrapRootElement = wrapRootElement;
--- packages/www/src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-    return (
-        <div>
-            Page Not Found
-        </div>
-    );
-}
