325aab678e41659f9015e7854f4f2312efa4f22d
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-15 05:01:44 +0500

    Step03- Add Header component in the app
    a:- insatlled
    	i:- materail ui Plugin the app using
    		1:- "yarn add gatsby-plugin-material-ui @material-ui/core @material-ui/icons @material-ui/styles mdi-material-ui --save"
    		2:- update gatsby-config.js
    b:- created
    	i:- src/components/Header.tsx for defining header component
    	ii:- src/components/header.css for defining styling for header component
    c:- updated
    	i:- src.pages/index.tsx to add header in the app

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+// Imports
+// React Imports
+import React from 'react';
+// Material UI Imports
+import { IconButton, Typography } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+// Styles Imports
+import './header.css';
+
+// Header Component
+const Header = () => {
+    //  Header Function Return
+    return (
+        <div className="root">
+            <AppBar position="static" elevation={1}>
+                <Toolbar className="transparent">
+                    <IconButton
+                        edge="start"
+                        aria-label="Shoe Store">
+                        <AssignmentIcon />
+                    </IconButton>
+                    <Typography variant="h5">
+                        TaskBox
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default Header; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function name() {
-    return(
+    return (
         <div>
-            Hello world!
+            <Header/>
         </div>
     )
-    
+
 }
