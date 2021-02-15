8dffb6d3aa41ae2a09efaa9ddc9dfb1ef7282dea
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-15 05:03:42 +0500

    Step04- Add Footer component in the app
    a:- created
    	i:- src/components/Footer.tsx for defining footer component
    	ii:- created src/components/footer.css for defining styling for footer component
    c:- updated
    	i:- src.pages/index.tsx to add footer in the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function name() {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     )
 
