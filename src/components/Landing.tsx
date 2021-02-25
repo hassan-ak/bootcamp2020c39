226a8b363bfffaa45fabdc4a45b20de45aacf066
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-02-25 17:54:13 +0500

    Step07- Netlify Authentication
    a:- Installed
    	a:- Netlify identity widget
    		i:- "yarn add netlify-identity-widget"
    b:- Update
    	a:- src\components\Landing.tsx to import netlify widget and initilized it on the page
    c:- Web
    	a:- from site and identity settings enable netlify identity

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,23 +1,27 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
-import './landing.css'
+import { Button } from "@material-ui/core";
+import React, { useEffect } from "react";
+import netlifyIdentity from "netlify-identity-widget";
+import "./landing.css";
 
 export const Landing = () => {
-    return (
-        <div className="landingContainer">
-            <div className="landingContent">
-                <h1>Manage your Todo's</h1>
-            </div>
-            <div className="buttonDiv">
-                <Button 
-                    className="landingButton"
-                    onClick={()=>{
-                        alert("clicked")
-                    }}
-                >
-                    Login
-                </Button>
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <div className='landingContainer'>
+      <div className='landingContent'>
+        <h1>Manage your Todo's</h1>
+      </div>
+      <div className='buttonDiv'>
+        <Button
+          className='landingButton'
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+};
